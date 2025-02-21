5a71c7d239e2536642e1c555b2e28e3076de9b05
0xedev <oxdeve@gmail.com>
2025-02-21 22:12:59 +0100

    frame v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import FrameSDK from "@farcaster/frame-sdk";
+import { createAppKit } from "@reown/appkit/react";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { projectId, metadata, networks, wagmiAdapter } from "./config/config";
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+function FarcasterFrameProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const init = async () => {
+      FrameSDK.actions.ready();
+    };
+
+    init();
+  }, []);
+
+  return <>{children}</>;
+}
+
+// Create a new query client
+const queryClient = new QueryClient();
+
+// General config object for AppKit
+const generalConfig = {
+  projectId,
+  metadata,
+  networks,
+};
+
+// Initialize AppKit modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  ...generalConfig,
+});
+
+// Render the app with WagmiProvider and QueryClientProvider
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <FarcasterFrameProvider>
+          <App />
+        </FarcasterFrameProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>
+);
