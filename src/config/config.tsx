9338668f16469873fbb34a7608713e15066c61f4
0xedev <oxdeve@gmail.com>
2025-02-27 13:38:11 +0100

    frame

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -2,41 +2,42 @@ import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { base } from "@reown/appkit/networks";
 import type { AppKitNetwork } from "@reown/appkit/networks";
 import { QueryClient } from "@tanstack/react-query";
-import {
-  coinbaseWallet,
-  injected,
-  safe,
-  walletConnect,
-} from "wagmi/connectors";
+// import {
+//   coinbaseWallet,
+//   injected,
+//   metaMask,
+//   safe,
+//   walletConnect,
+// } from "wagmi/connectors";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { createConfig, http } from "wagmi";
 import { createClient } from "viem";
 
-import logo from "../assets/logo.png";
+// import logo from "../assets/logo.png";
 
 // Define WC_PARAMS
-const WC_PARAMS = {
-  projectId: import.meta.env.VITE_PROJECT_ID, // Replace with your actual project ID
-  chainId: 8453, // Example: Ethereum Mainnet (1)
-  rpc: {
-    8453: "https://base-mainnet.g.alchemy.com/v2/os5WiDtgiyV3YXhsy2P-Cc0IX5IwFbYy", // Example RPC URL
-  },
-};
+// const WC_PARAMS = {
+//   projectId: import.meta.env.VITE_PROJECT_ID, // Replace with your actual project ID
+//   chainId: 8453, // Example: Ethereum Mainnet (1)
+//   rpc: {
+//     8453: "https://base-mainnet.g.alchemy.com/v2/os5WiDtgiyV3YXhsy2P-Cc0IX5IwFbYy", // Example RPC URL
+//   },
+// };
 
 export const wagmiConfig = createConfig({
   chains: [base],
   connectors: [
     farcasterFrame(),
-    injected(),
-    walletConnect(WC_PARAMS),
-    coinbaseWallet({
-      appName: "FlipIt",
+    // injected(),
+    // walletConnect(WC_PARAMS),
+    // coinbaseWallet({
+    //   appName: "FlipIt",
 
-      appLogoUrl: `https://flip-it-three.vercel.app/${logo}`,
-      reloadOnDisconnect: false,
-      enableMobileWalletLink: true,
-    }),
-    safe(),
+    //   appLogoUrl: `https://flip-it-three.vercel.app/${logo}`,
+    //   reloadOnDisconnect: false,
+    //   enableMobileWalletLink: true,
+    // }),
+    // safe(),
   ],
   client({ chain }) {
     return createClient({
